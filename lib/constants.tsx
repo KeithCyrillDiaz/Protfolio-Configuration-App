2368b0b21e6d917955cc62fa105e3c2e4d8bd2a3
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-10-25 13:10:42 +0800

    added social urls

---
+++ lib/constants.tsx
@@ -0,0 +1,5 @@
+export const SocialsUrl = {
+    facebook: "https://www.facebook.com/keith.diaz.58",
+    gitHub: "https://github.com/KeithCyrillDiaz",
+    linkedIn: "https://www.linkedin.com/in/keith-cyrill-diaz-637a5a2b2/"
+}
